610f31a78c506fc12fc01dbd60634a269f4620c0
karatisiva3012-beep <karatisiva3012@gmail.com>
2026-08-13 18:22:18 +0530

    Add files via upload

---
+++ Records/Week 3/PrimePalindrome.java
@@ -0,0 +1,34 @@
+package core_java;
+
+import java.util.Scanner;
+
+public class PrimePalindrome {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter a number: ");
+		int n = sc.nextInt();
+		int temp = n;
+		int reverse = 0;
+
+		while (temp > 0) {
+			int digit = temp % 10;
+			reverse = reverse * 10 + digit;
+			temp = temp / 10;
+		}
+		int count = 0;
+
+		for (int i = 1; i <= n; i++) {
+			if (n % i == 0) {
+				count++;
+			}
+		}
+
+		if (reverse == n && count == 2)
+			System.out.println(n + " is a Prime Palindrome");
+		else
+			System.out.println(n + " is not a Prime Palindrome");
+
+		sc.close();
+	}
+}
