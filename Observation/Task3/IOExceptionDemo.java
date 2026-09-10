94602a84c4a9772f4b2ea7f8272656c0059cb850
karatisiva3012-beep <karatisiva3012@gmail.com>
2026-09-10 19:31:39 +0530

    Add files via upload

---
+++ Observation/Task3/IOExceptionDemo.java
@@ -0,0 +1,29 @@
+package exceptionhandling;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+public class IOExceptionDemo {
+
+    public static void main(String[] args) {
+
+        try {
+
+            FileReader file = new FileReader("student.txt");
+
+            System.out.println("File Opened Successfully");
+
+            file.close();
+
+        } catch (IOException e) {
+
+            System.out.println("Exception Caught");
+            System.out.println("Input/Output Error");
+            System.out.println(e);
+
+        }
+
+        System.out.println("Program Continues");
+
+    }
+}
